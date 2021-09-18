8943e2093bccd3dd21fa2344f38f68b5ab9cd2c8
Amir <amir.anwar.said@gmail.com>
2021-09-18 17:17:22 +0200

    Change to Postgresl

--- sync/settings.py
+++ sync/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'sync.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "codasync",
+        "USER": "codabene",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
