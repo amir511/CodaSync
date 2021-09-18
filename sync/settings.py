76d6a81f0b3e9bfd06c6e9b5cb3004c4e7bea818
Amir <amir.anwar.said@gmail.com>
2021-09-18 19:33:48 +0200

    Add django rest framework and swagger docs

--- sync/settings.py
+++ sync/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party libs:
+    "rest_framework",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
